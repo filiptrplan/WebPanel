160cb8eb1a5a45568b8dc974a68ee43815f94dc7
Fomic Da Boss <tg.trplan@gmail.com>
2018-03-16 15:25:25 +0100

    finish constructor, add isAdmin()

--- WebPanel/inc/user.php
+++ WebPanel/inc/user.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'inc.php';
-require 'header.php';
 
 class User
 {
@@ -10,17 +9,38 @@ class User
     private $hwid;
     private $ban;
     private $type;
-
   
     public function __construct($data, $type)
     {
+        echo 'started';
         if ($type === 'id') {
             $rows = DB::select('SELECT * FROM users WHERE id=:id', array(':id' => $data));
-            var_dump($rows[0]);
+            $result = $rows[0];
+            $this->id = $result['id'];
+            $this->username = $result['username'];
+            $this->password = $result['password'];
+            $this->hwid = $result['hwid'];
+            $this->ban = $result['ban'];
+            $this->type = $result['type'];
         } elseif ($type === 'username') {
             $rows = DB::select('SELECT * FROM users WHERE username=:username', array(':username' => $data));
+            $result = $rows[0];
+            $this->id = $result['id'];
+            $this->username = $result['username'];
+            $this->password = $result['password'];
+            $this->hwid = $result['hwid'];
+            $this->ban = $result['ban'];
+            $this->type = $result['type'];
         } else {
             echo "Invalid Type!";
+        }
+    }
+
+    public function isAdmin(){
+        if($this->type === '100'){
+            return true;
+        }else{
+            return false;
         }
     }
 }
