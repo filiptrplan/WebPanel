4a9dd0c68b0dad884267b12f57a1f7c006785cc8
Fomic Da Boss <tg.trplan@gmail.com>
2018-03-18 09:42:28 +0100

    add manager class

---
+++ WebPanel/inc/manager.php
@@ -0,0 +1,26 @@
+<?php
+class Manager
+{
+    public static function banUser($user)
+    {
+        $id = $user->getId();
+        DB::query('UPDATE users SET ban=1 WHERE id=:id', array(':id' => $id));
+    }
+    public static function addUser($username, $password, $type)
+    {
+        DB::query(
+            'INSERT INTO users (username, password, type) VALUES (:username, :password, :type)',
+         array(':username' => $username, ':password' => $password, ':type' => $type)
+        );
+    }
+    public static function removeUser($user)
+    {
+        $id = $user->getId();
+        DB::query('DELETE FROM users WHERE id=:id', array(':id' => $id));
+    }
+    public static function pardonUser($user)
+    {
+        $id = $user->getId();
+        DB::query('UPDATE users SET ban=0 WHERE id=:id', array(':id' => $id));
+    }
+}
