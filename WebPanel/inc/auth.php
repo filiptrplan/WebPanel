01629f6123985457444cf1b65dbb967c001c4017
Fomic Da Boss <tg.trplan@gmail.com>
2018-03-19 16:17:21 +0100

    Auth Class - Login function

---
+++ WebPanel/inc/auth.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'inc.php';
+
+class Auth
+{
+    public static function login($username, $password)
+    {
+        $rows = DB::select('SELECT * FROM users WHERE username=:username', array(':username' => $username));
+        if (count($rows) == 0) {
+            return 'noexist';
+        }
+        $user = $rows[0];
+        $correctPassword = password_verify($password, $user['password']);
+        if ($correctPassword) {
+            return 'success';
+        } else {
+            return 'invalid';
+        }
+    }
+}
