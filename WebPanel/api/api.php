026b30d63a00f625332d8f6b98ec16fdb560273d
Fomic Da Boss <tg.trplan@gmail.com>
2018-03-20 09:18:47 +0100

    Api for external applications

---
+++ WebPanel/api/api.php
@@ -0,0 +1,35 @@
+<?php
+
+class Api{
+  private static function encrypt($username, $hwid, $status)
+  {
+    $encrypted = $username . $hwid . $status;
+    //Replace the part between these comments
+    $encrypted = base64_encode($encrypted);
+    $encrypted = md5($encrypted);
+    $encrypted = strrev($encrypted);
+    //End of comments
+    return $encrypted;
+  }
+
+  public static function checkLogin($reqUsername, $reqPassword, $reqHwid){
+    $loggedIn = Auth::login($reqUsername, $reqPassword);
+    if ($loggedIn == 'success') {
+      $user = new User($reqUsername, 'username');
+      if (!$user->isBanned()) {
+        if ($user->getHWID() == '') {
+          Manager::setHWID($user, $reqHwid);
+          return self::encrypt($reqUsername, $reqHwid, 'success');
+        } elseif ($user->getHWID() == $reqHwid) {
+          return self::encrypt($reqUsername, $reqHwid, 'success');
+        } else {
+          return self::encrypt($reqUsername, $reqHwid, 'wronghwid');
+        }
+      } else {
+        return self::encrypt($reqUsername, $reqHwid, 'banned');
+      }
+    } else {
+      return self::encrypt($reqUsername, $reqHwid, 'invalid');
+    }
+  }
+}
