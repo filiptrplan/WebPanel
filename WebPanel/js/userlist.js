53eb6c1a47418ee8b047a2781cc4407d4ae8e5d3
Filip Trplan <filip@fomic.net>
2018-03-31 21:26:58 +0200

    Search functions

--- WebPanel/js/userlist.js
+++ WebPanel/js/userlist.js
@@ -4,4 +4,19 @@ $(document).ready(function () {
     var id = $(this).data('id');
     $('.id-input').attr('value', id);
   });
+  $('#search-input').keyup(function (e) { 
+    let searchString = $('#search-input').val();
+    if(searchString != ''){
+      $('tbody tr').hide();
+      for(let node of $('tbody tr')){
+        node = $(node);
+        let username = node.find('.username-field');
+        if($(username).html().includes(searchString)){
+          node.show();
+        }
+      }
+    }else{
+      $('tbody tr').show();
+    }
+  });
 });
