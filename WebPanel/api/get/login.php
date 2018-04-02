5899d0fe2e69b0e43f3058d0c4afaa5e23b65acb
Filip Trplan <filip@fomic.net>
2018-04-02 12:00:46 +0200

    Add authkey vars

--- WebPanel/api/get/login.php
+++ WebPanel/api/get/login.php
@@ -2,10 +2,10 @@
 require_once '../../inc/inc.php';
 require_once '../api.php';
 
-if (isset($_GET['hwid']) && isset($_GET['username']) && isset($_GET['password'])) {
+if (isset($_GET['hwid']) && isset($_GET['username']) && isset($_GET['password']) && isset($_GET['authkey'])) {
   $reqHwid = $_GET['hwid'];
   $reqUsername = $_GET['username'];
   $reqPassword = $_GET['password'];
-
-  echo Api::checkLogin($reqUsername, $reqPassword, $reqHwid);
+  $authKey = $_GET['authkey'];
+  echo Api::checkLogin($reqUsername, $reqPassword, $reqHwid, $authKey);
 }
--- WebPanel/api/post/login.php
+++ WebPanel/api/post/login.php
@@ -3,10 +3,10 @@ require_once '../../inc/inc.php';
 require_once '../api.php';
 
 
-if (isset($_POST['hwid']) && isset($_POST['username']) && isset($_POST['password'])) {
+if (isset($_POST['hwid']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['authkey'])) {
   $reqHwid = $_POST['hwid'];
   $reqUsername = $_POST['username'];
   $reqPassword = $_POST['password'];
-
-  echo Api::checkLogin($reqUsername, $reqPassword, $reqHwid);
+  $authKey = $_POST['authkey'];
+  echo Api::checkLogin($reqUsername, $reqPassword, $reqHwid, $authKey);
 }
