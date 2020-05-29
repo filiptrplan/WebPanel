db620a7222e9898c42a015a0d48c20ff40316e84
unknown <tg.trplan@gmail.com>
2020-05-29 16:42:10 +0200

    complete wipe

--- WebPanel/inc/checksession.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'inc.php';
-if (!isset($_SESSION['user']) || !$_SESSION['user']->isAdmin() || $_SESSION['user']->isBanned()) {
-  header('Location: login.php');
-  exit;
-}
--- WebPanel/inc/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Config{
-  public static function get($name){
-    $config = parse_ini_file('config.ini');
-    return $config[$name];
-  }
-}
